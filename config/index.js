e542f6d7a2934855eb103f83b49891ec1047d394
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2018-01-09 14:42:12 +0800

    Build Static Files

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'd3-practice-vue-svg/dist/',
 
     /**
      * Source Maps
